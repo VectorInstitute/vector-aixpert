7a6bfb9c88cf17d21d9067e8ca181fcf8b50cd57
unknown <your-email@example.com>
2025-10-01 14:27:53 -0400

    swapping the dictionaries

--- src/aixpert/data_generation/agent_pipeline/utils.py
+++ src/aixpert/data_generation/agent_pipeline/utils.py
@@ -10,7 +10,7 @@ def var_to_dict_prompts() -> tuple[dict, dict]:
     for name, value in vars(system_prompt).items():
         if not name.startswith("__"):  # skip built-ins
             if "prompt_vqa" in name:
-                prompt_dict[name] = value
-            else:
                 prompt_vqa_dict[name] = value
+            else:
+                prompt_dict[name] = value
     return prompt_dict, prompt_vqa_dict
