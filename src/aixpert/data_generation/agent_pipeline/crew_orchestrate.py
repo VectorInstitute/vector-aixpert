32da9342c51cce33e2ed2f66a8369be76eb5399e
amasin2111 <113543821+amasin2111@users.noreply.github.com>
2025-10-17 14:29:21 -0400

    Merge pull request #35 from VectorInstitute/feature/agentic_workflow
    Metadata Generation Pipeline

--- src/aixpert/data_generation/agent_pipeline/crew_orchestrate.py
+++ src/aixpert/data_generation/agent_pipeline/crew_orchestrate.py
@@ -5,7 +5,11 @@ import os
 import re
 from typing import Any, Union
 
-from agent import create_prompt_agents_and_task
+from agent import (
+    create_metadata_agent,
+    create_metadata_tasks,
+    create_prompt_agents_and_task,
+)
 from crewai import Crew
 from custom_llm import CustomLLM
 from flows.image_generation_flow import GeminiImageGenerationFlow
@@ -16,6 +20,7 @@ from utils import load_prompt, read_directory, var_to_dict_prompts
 current_script_dir = os.path.dirname(os.path.abspath(__file__))
 prompts_base_path = os.path.join(current_script_dir, "prompts")
 base_image_folder = os.path.join(current_script_dir, "images")
+metadata_base_path = os.path.join(current_script_dir, "metadata")
 
 
 def my_local_model(prompt: str, temperature: float = 0.7) -> str:
@@ -29,7 +34,7 @@ llm = CustomLLM(my_local_model)
 def prompt_generation_flow(llm: Any) -> Union[str, list, None]:
     """Flow for creating the prompts."""
     prompts_base_path = "./prompts/"
-    prompts, prompts_vqa = var_to_dict_prompts()
+    prompts, prompts_vqa, prompt_metadata = var_to_dict_prompts()
     print(prompts)
     # exit()
     task_list = []
@@ -94,6 +99,69 @@ def image_generation_flow(llm: str) -> None:
             flow.kickoff()
 
 
+def metadata_generation_pipeline(llm: Any) -> None:
+    """Pipeline for creating the metadata from the prompt."""
+    prompts, prompts_vqa, prompt_metadata = var_to_dict_prompts()
+    prompt_file_names = read_directory(prompts_base_path)
+    # for prompt_name, prompt in prompts_metadata.items():
+    llm_model = load_text_llm("gemini")
+    metadata_agent = create_metadata_agent(
+        role="AI Assistant", goal=prompt_metadata["prompt_metadata"], llm=llm_model
+    )
+    for file in prompt_file_names:
+        task_list = []
+        # print(file)
+        prompts = load_prompt(file)
+        # print(len(prompts))
+        for i, elem in enumerate(prompts):
+            domain = elem["domain"]
+            risk = elem["risk"]
+            task_list.append(
+                create_metadata_tasks(metadata_agent, elem["image_prompt"])
+            )
+            crew_create_and_launch(task_list)
+            for _idx, task in enumerate(task_list):
+                if isinstance(task.output.raw, list):
+                    prompts = [p.strip() for p in task.output.raw if p]
+                else:
+                    prompts = [task.output.raw.strip()] if task.output.raw else []
+            content = "\n".join(prompts) if isinstance(prompts, list) else prompts
+            metadata = re.findall(r"```json(.*)```", content, re.DOTALL)
+            assert len(metadata) == 1, "there should be only one metadata generated"
+            try:
+                metadata = json.loads(metadata[0])
+            except json.JSONDecodeError as e:
+                print(f"Skipping invalid JSON for entry {i} : {e}")
+                continue
+            output_filename = f"{domain}_{risk}_image_{i + 1}.png"
+            image_folder = os.path.join(base_image_folder, llm)
+            folder_name = os.path.join(image_folder, f"{domain}_{risk}_images")
+            output_filename = f"{domain}_{risk}_image_{i + 1}.png"
+            #output_filename = os.path.join(folder_name, output_filename)
+            output_filename_path = os.path.join(folder_name, output_filename)
+            if not os.path.exists(output_filename_path):
+                print("image file doesn't exist so skipping")
+                continue
+            os.makedirs(metadata_base_path, exist_ok=True)
+            metadata_llm_path = os.path.join(metadata_base_path, llm)
+            os.makedirs(metadata_llm_path, exist_ok=True)
+            output_jsonl_path = os.path.join(
+                metadata_llm_path,
+                elem["domain"] + "_" + elem["risk"] + "_metadata.jsonl",
+            )
+            parsed = {
+                "domain": elem["domain"],
+                "risk": elem["risk"],
+                "image_prompt": elem["image_prompt"],
+                "image_path": output_filename_path,
+                "metadata": metadata["metadata"],
+            }
+            with open(output_jsonl_path, "a", encoding="utf-8") as f:
+                json_line = json.dumps(parsed, ensure_ascii=False)
+                f.write(json_line + "\n")
+            print("Metadata saved successfully.")
+
+
 def crew_create_and_launch(task_list: list) -> None:
     """Create crew and launch."""
     crew = Crew(tasks=task_list, verbose=False)
@@ -102,5 +170,5 @@ def crew_create_and_launch(task_list: list) -> None:
 
 if __name__ == "__main__":
     # result = prompt_generation_flow(llm)
-    image_generation_flow("gemini")
+    metadata_generation_pipeline("gemini")
     # print(len(result))
